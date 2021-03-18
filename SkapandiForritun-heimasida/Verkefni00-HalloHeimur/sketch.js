da97b9346a02c0c61fec82d52e11a4c7f4643f79
JonRagnar04 <80835837+JonRagnar04@users.noreply.github.com>
2021-03-18 10:57:15 +0000

    Prófun í Verkefni00

--- SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
+++ SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
@@ -1,5 +1,13 @@
 // Hér kemur kóðinn þinn:
-
+function setup(){
+    createCanvas(300,300);
+    background(255,0,0);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(20);
+    text("Halló heimur",150,40);
+}
 function setup() {
   createCanvas(400, 400);
   background(255,200,0);
