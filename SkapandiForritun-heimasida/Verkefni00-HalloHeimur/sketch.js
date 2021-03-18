3a425d97ab2682ebfdcda8affd91d851a5830082
JonRagnar04 <80835837+JonRagnar04@users.noreply.github.com>
2021-03-18 11:54:18 +0000

    Nýtt look

--- SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
+++ SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
@@ -1,4 +1,5 @@
 // Hér kemur kóðinn þinn:
+
 function setup(){
     createCanvas(300,300);
     background(255,0,0);
@@ -7,14 +8,4 @@ function setup(){
     fill(0);
     textSize(20);
     text("Halló heimur",150,40);
-}
-function setup() {
-  createCanvas(400, 400);
-  background(255,200,0);
-  frameRate(4);
-}
-
-function draw() {
-  fill(random(0,255));
-  ellipse(200,200,50,50);
 }
