27a638e3a041406a6451fb113b364ba6f038c26f
JonRagnar04 <80835837+JonRagnar04@users.noreply.github.com>
2021-03-22 15:55:25 +0000

    Breyting á litum og hringjum

--- SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
+++ SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
@@ -1,11 +1,13 @@
 // Hér kemur kóðinn þinn:
 
 function setup(){
-    createCanvas(300,300);
-    background(255,0,0);
+    createCanvas(400,400);
+    background(255,255,0);
     fill(150);
     ellipse(100,100,50,50);
     fill(0);
+    ellipse(150,100,75,75);
+    fill(0,255,207);
     textSize(20);
     text("Halló heimur",150,40);
 }
