a9c14fc2cbf7827bca528d765b21c6c860880c37
JonRagnar04 <80835837+JonRagnar04@users.noreply.github.com>
2021-03-22 16:49:10 +0000

    Prufun

--- SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
+++ SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
@@ -7,7 +7,14 @@ function setup(){
     ellipse(100,100,50,50);
     fill(0,255,207);
     ellipse(250,200,75,75);
+    fill(255,0,170);
+    rect(400,100,65,50);
+    fill(255,0,0);
+    rect(400,400,30,70);
     fill(0);
     textSize(20);
     text("Halló heimur",150,40);
+    fill(0)
+    textSize(20);
+    text("Í dag er yndislegur dagur!",285,450)
 }
