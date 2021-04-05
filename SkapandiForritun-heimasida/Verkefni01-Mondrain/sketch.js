6ef4fe2dd72f2f3773067839cfa11f1dadc97174
JonRagnar04 <80835837+JonRagnar04@users.noreply.github.com>
2021-04-05 21:41:07 +0000

    Quick save

--- SkapandiForritun-heimasida/Verkefni01-Mondrain/sketch.js
+++ SkapandiForritun-heimasida/Verkefni01-Mondrain/sketch.js
@@ -3,6 +3,25 @@
 function setup(){
     createCanvas(225,225);
     background(0,0,0);
+    noStroke();
     fill(229,49,35);
-    rect(0,0,94,120);
+    rect(0,0,95,120);//Rauði Rétthyrningurinn
+    noStroke();
+    fill(236,237,232);
+    rect(100,0,125,120);//Hvíti Rétthyrningurinn uppi í hægra horninu
+    noStroke();
+    fill(233,235,230);
+    rect(0,125,95,101);//Hvíti Rétthyrningurinn niðri í vinstra horninu
+    noStroke();
+    fill(237,239,228);
+    rect(100,125,105,87);//Hvíti Rétthyrningurinn á milli bláa og gula
+    noStroke();
+    fill(252,201,43);
+    rect(100,217,59,12);//Guli Rétthyrningurinn
+    noStroke();
+    fill(0,67,141);
+    rect(210,125,15,37);//Blái Rétthyrningurinn
+    noStroke();
+    fill(232,234,229);
+    rect(210,167,15,63);
 }
