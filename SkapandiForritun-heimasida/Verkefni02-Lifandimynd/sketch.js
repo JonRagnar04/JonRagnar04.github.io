6651046e65e1fc4574dd3264edd013665f88f7d9
JonRagnar04 <80835837+JonRagnar04@users.noreply.github.com>
2021-04-15 20:35:06 +0000

    Verkefni02

---
+++ SkapandiForritun-heimasida/Verkefni02-Lifandimynd/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+      createCanvas(200, 300);
+      noStroke();
+    }
+
+    function draw() {
+      background(136, 86, 0);
+
+//Steinn úr eldfjalli
+      beginShape();
+      fill(175,175,175);
+      vertex(50,150);
+      vertex(80,110);
+      vertex(100,100);
+      vertex(140,130);
+      vertex(145, 150);
+      vertex(130,170);
+      vertex(100,200);
+      vertex(80,200);
+      endShape();
+    }
